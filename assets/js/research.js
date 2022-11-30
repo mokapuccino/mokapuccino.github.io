b7a396433da2457cc938f0b57e37f77f44f68385
Moka <94351935+mokapuccino@users.noreply.github.com>
2022-11-30 15:17:57 +0100

    Add files via upload

--- assets/js/research.js
+++ assets/js/research.js
@@ -13,6 +13,15 @@ const research = [
     absbox: "absPopup0",
   },
   {
+    title:
+      "Mes compétences métier",
+    desp: "<p>Je n’ai pas encore d’idée exacte du métier que je souhaite exercer. Cependant, je souhaite travailler dans l’informatique et plus particulièrement exercer un métier en lien avec l’Intelligence Artificielle ou/et le Développement Web. </p><p>Voici une liste non exhaustive des compétences nécessaires à cette évolution professionnelle. </p><p>Savoir : </p><p><li>Coder</li><li>Acquérir rapidement et de manière autonome des connaissances</li><li>Faire preuve de créativité</li><li>S’auto-former, veille permanente</li><li>S'engager et agir en autonomie</li><li>Travailler en équipe et faire partager ses connaissances</li><li>Rédiger, synthétiser des documents</li></p>",
+    image: "assets/images/research-page/job.png", 
+    abstract:
+    "<h3 style='text-align' :justify>Mes compétences métiers :</h3><p>Je n’ai pas encore d’idée exacte du métier que je souhaite exercer. Cependant, je souhaite travailler dans l’informatique et plus particulièrement exercer un métier en lien avec l’Intelligence Artificielle ou le Développement Web. </p><p>Voici une liste non exhaustive des compétences nécessaires à cette évolution professionnelle : </p><p><li>Coder</li><li>Acquérir rapidement et de manière autonome des connaissances</li><li>Faire preuve de créativité</li><li>S’auto-former, veille permanente</li><li>S'engager et agir en autonomie</li><li>Travailler en équipe et faire partager ses connaissances</li><li>Rédiger, synthétiser des documents</li></p>",
+    absbox: "absPopup0",
+  },
+  {
     title: "Mes Savoirs",
     desp: "<p class='amb'><li><h5>Globaux :</h5> Pack Office, Linux, Windows</li><br><li><h5>Programmation : </h5>Python, C#, C++, C, UML, Java, LaTeX</li><br><li><h5>IA : </h5>Machine Learning, Visualisation de données, Traitement d'images</li><br><li><h5>Réseau :</h5> CCNA 1 & 2, Packet Tracer</li><br><li><h5>Developpement Web : </h5> HTML, CSS, Javascript</li><br><li><h5>Gestion de projet : </h5> Méthodes SCRUM / Agile</li></p><li><h5>Langues, anglais : </h5>niveau C1 (score TOEIC 960/990)</li><br><li><h5>Langues, espagnol : </h5>niveau B1 </li><br>",
     image: "assets/images/research-page/K1.png",
